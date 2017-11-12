f7e5e94a0b22a57e29256c549a7ee27593481aa3
jonnyboy11494 <jonathan.yu.94@gmail.com>
2017-11-12 21:10:09 +1100

    show watchlist working

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/graph/<list_id>')
+def show_graphs(list_id):
+    if list_id == "1":
+        watchlist = ["TLS.AX", "WFD.AX"]
+    else:
+        watchlist = []
+    print(watchlist)
+    return render_template("watchlist.html", watchlist=watchlist)
